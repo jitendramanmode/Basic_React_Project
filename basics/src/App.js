6964413a7878d3c9397df4af35a35d01d859b6a0
Jitendra-Manmode <131594721+Jitendra-Manmode@users.noreply.github.com>
2024-12-29 13:29:27 +0530

    App comp

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+
+  return (
+    <h1>Hello</h1>
+  );
+}
+export default App;
