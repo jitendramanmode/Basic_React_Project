f4232ebfd56f9a82ce53f2dd04cf8d6be12d20e8
Jitendra-Manmode <131594721+Jitendra-Manmode@users.noreply.github.com>
2024-12-29 14:19:52 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,8 +1,18 @@
+import Home from "./Home"
+import About from "./About"
+import Contact from "./Contact"
+import Header from "./Header";
 
 function App() {
 
   return (
-    <h1>Hello</h1>
+    <div>
+    <h1>Hello from ReactJS</h1>
+    <Home name = {'Jitu'} age = {23} />
+    <About />
+    <Contact />
+    <Header />
+    </div>
   );
 }
 export default App;
